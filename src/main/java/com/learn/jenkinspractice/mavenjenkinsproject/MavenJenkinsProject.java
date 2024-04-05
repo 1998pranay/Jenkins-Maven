54606eab0df73d158e8314d3bdfc5a4411ea0880
Pranay Nimunkar <nimunkarpranay12345@gmail.com>
2024-04-05 23:50:29 +0530

    first commit

---
+++ src/main/java/com/learn/jenkinspractice/mavenjenkinsproject/MavenJenkinsProject.java
@@ -0,0 +1,9 @@
+
+package com.learn.jenkinspractice.mavenjenkinsproject;
+
+class MavenJenkinsProject {
+
+    public static void main(String[] args) {
+        System.out.println("Maven Jenkins Project ");
+    }
+}
