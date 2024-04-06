fbd854337a047b01bf772192f1831d6a6363ca90
Pranay Nimunkar <121708534+1998pranay@users.noreply.github.com>
2024-04-06 15:41:32 +0530

    MavenJenkinsProject.java

--- src/main/java/com/learn/jenkinspractice/mavenjenkinsproject/MavenJenkinsProject.java
+++ src/main/java/com/learn/jenkinspractice/mavenjenkinsproject/MavenJenkinsProject.java
@@ -5,5 +5,6 @@ class MavenJenkinsProject {
 
     public static void main(String[] args) {
         System.out.println("Maven Jenkins Project ");
+        System.out.println("Maven Jenkins Project One");
     }
 }
