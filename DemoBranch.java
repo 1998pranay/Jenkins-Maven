55910e4b4111ef101cbb0d9e449391a11fc8d337
Pranay Nimunkar <121708534+1998pranay@users.noreply.github.com>
2024-04-06 11:51:01 +0530

    Add files via upload

---
+++ DemoBranch.java
@@ -0,0 +1,9 @@
+
+package com.learn.jenkinspractice.mavenjenkinsproject;
+
+
+public class DemoBranch {
+    public static void main(String[] args) {
+        System.out.println("This is demo branch");
+    }
+}
